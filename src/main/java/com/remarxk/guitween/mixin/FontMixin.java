146c9b4b4dba1b33fcb2c3a7bbebc0e3d20cdce8
remarxk <qq15976230339@outlook.com>
2026-05-18 23:04:14 +0800

    修复字体透明度不生效的问题

--- src/main/java/com/remarxk/guitween/mixin/FontMixin.java
+++ src/main/java/com/remarxk/guitween/mixin/FontMixin.java
@@ -4,19 +4,41 @@ import com.remarxk.guitween.GUITweenUtility;
 import net.minecraft.client.gui.Font;
 import net.minecraftforge.client.extensions.IForgeFont;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(Font.class)
 public abstract class FontMixin implements IForgeFont {
     @ModifyArg(
+            method = "drawInternal(Ljava/lang/String;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/gui/Font$DisplayMode;IIZ)I",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/Font;adjustColor(I)I"
+            ),
+            index = 0
+    )
+    public int changeColor1(int color) {
+        return gUITween$changeColorInternal(color);
+    }
+
+    @ModifyArg(
             method = "drawInternal(Lnet/minecraft/util/FormattedCharSequence;FFIZLorg/joml/Matrix4f;Lnet/minecraft/client/renderer/MultiBufferSource;Lnet/minecraft/client/gui/Font$DisplayMode;II)I",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/Font;adjustColor(I)I"),
             index = 0
     )
-    public int changeColor(int color) {
+    public int changeColor2(int color) {
+        return gUITween$changeColorInternal(color);
+    }
+
+    @Unique
+    private int gUITween$changeColorInternal(int color) {
         if (GUITweenUtility.hasFontAlpha()) {
-            int a = (int)(((color >> 24) & 0xFF) * GUITweenUtility.peekFontAlpha());
+            int a = (color >> 24) & 0xFF;
+            if (a == 0) {
+                a = 255;
+            }
+            a = (int) (a * GUITweenUtility.peekFontAlpha());
             color = (a << 24) | (color & 0x00FFFFFF);
         }
 
